cf75c0d36c52943577415a98d77a02e889929feb
Andy <flyandi@yahoo.com>
2017-08-26 23:08:12 -0700

    Removes createJSModules since it's no longer required in 0.47

--- android/src/main/java/com/github/xfumihiro/react_native_image_to_base64/ImageToBase64Package.java
+++ android/src/main/java/com/github/xfumihiro/react_native_image_to_base64/ImageToBase64Package.java
@@ -22,10 +22,6 @@ public class ImageToBase64Package implements ReactPackage {
     return modules;
   }
 
-  @Override
-  public List<Class<? extends JavaScriptModule>> createJSModules() {
-    return Collections.emptyList();
-  }
 
   @Override
   public List<ViewManager> createViewManagers(ReactApplicationContext reactContext) {
